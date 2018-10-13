efd2bccbb1cdc335b9c98ad61c3c3318a5b07b83
Dave Goddard <dave@goddard.id.au>
2018-10-13 17:37:58 -0300

    add some unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::Read, cmp};
+use std::{cmp, thread, time};
 use crossterm::{Screen, cursor};
 
 fn gen(n: usize) -> Vec<Vec<bool>> {
@@ -8,9 +8,9 @@ fn gen(n: usize) -> Vec<Vec<bool>> {
 fn num_alive_neighbours(old: &Vec<Vec<bool>>, x: usize, y: usize) -> u8 {
     let mut n = 0;
     let max : usize = old.len();
-    for yp in (cmp::max(y, 1)-1)..cmp::min(y+1,max) {
-        for xp in (cmp::max(x, 1)-1)..cmp::min(x+1,max) {
-            if old[xp][yp] {
+    for yp in (cmp::max(y, 1)-1)..=cmp::min(y+1,max-1) {
+        for xp in (cmp::max(x, 1)-1)..=cmp::min(x+1,max-1) {
+            if old[xp][yp] && (xp != x || yp != y) {
                 n+=1;
             }
         }
@@ -31,11 +31,7 @@ fn next(old: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
 fn print(population: &Vec<Vec<bool>>) {
     for row in population.iter() {
         for cell in row.iter() {
-            if *cell {
-                print!("🀫")
-            } else {
-                print!("🀆")
-            }
+            print!("{}", if *cell { "🀫" } else { "🀆" });
         }
         println!("");
     }
@@ -44,13 +40,32 @@ fn print(population: &Vec<Vec<bool>>) {
 fn main() {
     let screen = Screen::default();
     let mut cursor = cursor(&screen);
+    let wait_duration = time::Duration::from_millis(300);
 
     let mut population = gen(20);
     loop {
         print(&population);
-        std::io::stdin().bytes().next();
+        thread::sleep(wait_duration);
         population = next(population);
 
-        cursor.move_up(population.len() as u16 + 1);
+        cursor.move_up(population.len() as u16);
     };
+}
+
+#[test]
+fn num_alive_when_all_are_true() {
+    let pop = vec![ vec![true, true, true], vec![true, true, true], vec![true, true, true] ];
+    assert_eq!(num_alive_neighbours(&pop, 0, 0), 3);
+    assert_eq!(num_alive_neighbours(&pop, 2, 2), 3);
+    assert_eq!(num_alive_neighbours(&pop, 1, 1), 8);
+    assert_eq!(num_alive_neighbours(&pop, 1, 0), 5);
+}
+
+#[test]
+fn num_alive_when_all_are_false() {
+    let pop = vec![ vec![false, false, false], vec![false, false, false], vec![false, false, false] ];
+    assert_eq!(num_alive_neighbours(&pop, 0, 0), 0);
+    assert_eq!(num_alive_neighbours(&pop, 2, 2), 0);
+    assert_eq!(num_alive_neighbours(&pop, 1, 1), 0);
+    assert_eq!(num_alive_neighbours(&pop, 1, 0), 0);
 }
