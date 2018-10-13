fe025ea224cb848bb6633c1b2b90e4b4043f5e54
Dave Goddard <dave@goddard.id.au>
2018-10-13 17:18:48 -0300

    allow different sizes of board

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
 use std::{io::Read, cmp};
 use crossterm::{Screen, cursor};
 
-fn gen() -> [[bool; 10]; 10] {
-	let mut population: [[bool; 10]; 10] = [[false; 10]; 10];;
-	for y in 0..10 {
-		for x in 0..10 {
-			if rand::random::<f32>() < 0.3 {
-				population[x][y] = true;
-			}
-		}
-	}
-	population
+fn gen(n: usize) -> Vec<Vec<bool>> {
+    (0..n).map(|_| (0..n).map(|_| rand::random::<f32>() < 0.3).collect()).collect()
 }
 
-fn num_alive_neighbours(old: [[bool; 10]; 10], x: usize, y: usize) -> u8 {
+fn num_alive_neighbours(old: &Vec<Vec<bool>>, x: usize, y: usize) -> u8 {
     let mut n = 0;
-    for yp in (cmp::max(y, 1)-1)..=cmp::min(y+1,10-1) {
-        for xp in (cmp::max(x, 1)-1)..=cmp::min(x+1,10-1) {
+    let max : usize = old.len();
+    for yp in (cmp::max(y, 1)-1)..cmp::min(y+1,max) {
+        for xp in (cmp::max(x, 1)-1)..cmp::min(x+1,max) {
             if old[xp][yp] {
                 n+=1;
             }
@@ -25,42 +18,39 @@ fn num_alive_neighbours(old: [[bool; 10]; 10], x: usize, y: usize) -> u8 {
     n
 }
 
-fn next(old: [[bool; 10]; 10]) -> [[bool; 10]; 10] {
-	let mut population: [[bool; 10]; 10] = [[false; 10]; 10];;
-	for y in 0..10 {
-		for x in 0..10 {
-			let num_alive = num_alive_neighbours(old, x, y);
-			if (old[x][y] && num_alive == 2) || num_alive == 3 {
-                            population[x][y] = true;
-                        }
-		}
-	}
-	population
+fn next(old: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+    let max : usize = old.len();
+    (0..max).map(|y|
+                (0..max).map(|x| {
+                    let num_alive = num_alive_neighbours(&old, x, y);
+                    (old[x][y] && num_alive == 2) || num_alive == 3
+                }).collect()
+    ).collect()
 }
 
-fn print(population: [[bool; 10]; 10]) {
-	for (_y, row) in population.iter().enumerate() {
-		for (_x, cell) in row.iter().enumerate() {
-			if *cell {
-				print!("🀫")
-			} else {
-				print!("🀆")
-			}
-		}
-		println!("");
-	}
+fn print(population: &Vec<Vec<bool>>) {
+    for row in population.iter() {
+        for cell in row.iter() {
+            if *cell {
+                print!("🀫")
+            } else {
+                print!("🀆")
+            }
+        }
+        println!("");
+    }
 }
 
 fn main() {
     let screen = Screen::default();
     let mut cursor = cursor(&screen);
 
-    let mut population = gen();
+    let mut population = gen(20);
     loop {
-        print(population);
+        print(&population);
         std::io::stdin().bytes().next();
         population = next(population);
 
-        cursor.move_up(11);
+        cursor.move_up(population.len() as u16 + 1);
     };
 }
